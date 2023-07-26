a07abee2d4877da1884c9b9bc1e4fc7d0ad54977
Choufleur235 <bidaultchloe@gmail.com>
2023-07-26 19:17:24 +0100

    Installed disabledReactDevTools

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import App from "./Components/App";
+import { disableReactDevTools} from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
